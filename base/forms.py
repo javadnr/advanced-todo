30967ef8805d20e619771e59c6eb9c3bf045236e
javadnr <javadnamavar554@gmail.com>
2023-12-01 13:50:09 +0330

    gitignore added

--- base/forms.py
+++ base/forms.py
@@ -73,7 +73,7 @@ class PasswordChangeform(PasswordChangeForm):
 class UpdateUserForm(forms.ModelForm): 
     username = forms.CharField(max_length=100,required=True,widget=forms.TextInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-
+ 
     class Meta:
         model = User
         fields = ['username', 'email']
